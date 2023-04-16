3c0a5dfb1bd22ae50df0e645be005a43f41fa609
PY-14 <88144261+CYTH1@users.noreply.github.com>
2023-04-16 21:35:36 +0100

    Update run2.py

--- run2.py
+++ run2.py
@@ -13,7 +13,7 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")
-    from DUMP import main
+    from DUMPER import main
     main_menu()
 elif bit == '32bit':
     print("\x1b[1;91mSorry Brother Your Mobile Not Support This Tools")
